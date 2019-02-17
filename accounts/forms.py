45cb0d855916b3d05d54b710f9f6cd85a7632263
Suman Chakraborty <sumanc820@gmail.com>
2019-02-17 13:34:20 +0530

    fully working accounts app

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,21 @@
-from django.contrib.auth import get_user_model
+from django import forms
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
 class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Email')
 
     class Meta:
         fields = ('username', 'email', 'password1', 'password2')
-        model = get_user_model()
+        model = User
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
 
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 from . import forms
 
@@ -8,6 +9,7 @@ from . import forms
 
 
 class SignUp(CreateView):
-    form_class = forms.UserCreateForm
+    # form_class = forms.UserCreateForm
+    form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
